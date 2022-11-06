0ec318cb8890f2bd91a508c385cf3e93582514fc
Parallels <kravtandr@gmail.com>
2022-11-06 22:11:58 +0300

    initial

---
+++ build/abot_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/parallels/ros/src/abot_description
+# Build directory: /home/parallels/ros/build/abot_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
